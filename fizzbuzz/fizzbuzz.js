7d621e265257d35965ee5d6dcb668a221f14bca2
Elayna <edbird1@yahoo.com>
2015-10-08 09:36:01 -0600

    Update Code to FizzBuzz

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -2,7 +2,21 @@
 
 var fizzbuzz = {
     getResult: function (passIn){
-        return passIn;
+        if (passIn === 0){
+            return 0;
+        }
+        else if (passIn % 15 === 0){
+            return "fizzbuzz";
+        }
+        else if (passIn % 3 === 0){
+            return "fizz";
+        }
+        else if (passIn % 5 === 0){
+            return "buzz";
+        }
+        else {
+            return passIn;
+        }
     }
 };
 
--- fizzbuzz/spec/fizzbuzz.spec.js
+++ fizzbuzz/spec/fizzbuzz.spec.js
@@ -10,6 +10,26 @@ var fizzbuzz = require("../fizzbuzz.js").fizzbuzz;
         it("should return 0 given 0", function(){
             expect(fizzbuzz.getResult(0)).toEqual(0);
         });
+    
+        it("should return 1 given 1", function(){
+            expect(fizzbuzz.getResult(1)).toEqual(1);
+        });
+    
+        it("should return 2 given 2", function(){
+            expect(fizzbuzz.getResult(2)).toEqual(2);
+        });
+    
+        it("should return fizz given 3", function(){
+            expect(fizzbuzz.getResult(3)).toEqual("fizz");
+        });
+    
+        it("should return buzz given 5", function(){
+            expect(fizzbuzz.getResult(5)).toEqual("buzz");
+        });
+    
+        it("should return fizzbuzz given 15", function(){
+            expect(fizzbuzz.getResult(15)).toEqual("fizzbuzz");
+        });
         
     });
 })();
