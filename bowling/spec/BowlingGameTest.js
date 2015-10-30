48ee19d9495f669c1b80f5fa7e664d4958983634
edbird1 <edbird1@yahoo.com>
2015-10-30 10:07:51 -0600

    TDD Testing

---
+++ bowling/spec/BowlingGameTest.js
@@ -0,0 +1,67 @@
+/*globals require, describe, it, expect, BowlingGame*/
+
+describe('The Bowling Game', function () {
+
+    var game;
+
+    beforeEach(function () {
+        game = new BowlingGame();
+    });
+
+    it('can roll gutter game', function () {
+        rollMany(0, 20);
+        expect(game.score()).toBe(0);
+    });
+
+    it('can roll all ones', function () {
+        rollMany(1, 20);
+        expect(game.score()).toBe(20);
+    });
+
+    it('can roll a spare', function () {
+        game.roll(5);
+        game.roll(5);
+        game.roll(3);
+        rollMany(0, 17);
+        expect(game.score()).toBe(16);
+    });
+
+    it('can roll lots of spares', function () {
+        game.roll(5);
+        game.roll(5);
+        //+11
+        game.roll(1);
+        game.roll(9);
+        //+15
+        //=26
+        game.roll(5);
+        game.roll(5);
+        //+13
+        //=39
+        game.roll(3);
+        //+3
+        //=42
+        rollMany(0, 13);
+        expect(game.score()).toBe(42);
+    });
+
+    it('can roll a strike', function () {
+        game.roll(10);
+        game.roll(3);
+        game.roll(5);
+        rollMany(0, 16);
+        expect(game.score()).toBe(26);
+    });
+
+    it('can roll a perfect game', function () {
+        rollMany(10, 12);
+        expect(game.score()).toBe(300);
+    });
+
+    var rollMany = function (pins, rolls) {
+        for (var i = 0; i < rolls; i++) {
+            game.roll(pins);
+        }
+    };
+
+});
