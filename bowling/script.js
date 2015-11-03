a3df68baaaa82a312f96a9d266e90b163986b44b
edbird1 <edbird1@yahoo.com>
2015-11-03 08:04:41 -0700

    Add On-Click Functionality

--- bowling/script.js
+++ bowling/script.js
@@ -1,5 +1,16 @@
+/*globals document*/
 var value = function (e) {
     return e;
 };
 
-document.getElementById('roll+i').innerHTML(value);
+function rollValue(e) {
+    var i = 1;
+    if (i <= 20 && e !== 10) {
+        document.getElementById('roll' + i).innerHTML = e;
+        i++;
+    } else if (i % 2 !== 0 && e == 10) {
+        i++;
+        document.getElementById("roll" + 1).innerHTML = "X";
+        i++;
+    }
+}
