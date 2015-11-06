ad8cfcc165457c10d9c39f85275c38c38e0e0fe4
MATC <MATC>
2015-11-06 10:29:25 -0700

    Add function for hidding buttons

--- bowling/script.js
+++ bowling/script.js
@@ -1,20 +1,36 @@
 /*globals document*/
-var value = function (e) {
-    return e;
+var hidden = false;
+var i = 1;
+
+var isHidden = function (obj) {
+    hidden = !hidden;
+    if (hidden) {
+        document.getElementById(obj).style.visibility = 'hidden';
+    } else {
+        document.getElementById(obj).style.visibility = 'visible';
+    }
 };
 
-    var i = 1;
-
-function rollValue (pins) {
+function rollValue(pins) {
 
     if (i <= 21 && pins !== '10') {
         document.getElementById('roll' + i).innerHTML = pins;
+        document.getElementById('button10').innerHTML = '/';
         i++;
-    } else if (i % 2 && pins == '10'){
-        document.getElementById('roll' + i).innerHTML = "X";
-        i += 2;
+        isHidden('testbutton');
+    } else if (i == 20 && pins == '10') {
+        document.getElementById('roll' + i).innerHTML = 'X';
+        i += 1;
+    } else if (i % 2 && pins == '10') {
+        document.getElementById('roll' + i).innerHTML = 'X';
+        if (i == 19) {
+            i += 1;
+        } else {
+            i += 2;
+        }
+
     }
-    
+
 }
 
 console.log(2 % 2);
